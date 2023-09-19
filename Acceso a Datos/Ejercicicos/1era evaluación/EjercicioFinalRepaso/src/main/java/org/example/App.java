9177298d92916bfc8aca449b77514633a4931399
DVerdes <144799801+DVerdes@users.noreply.github.com>
2023-09-19 11:30:33 +0200

    Update ejercicios vehículos

--- Acceso a Datos/Ejercicicos/1era evaluación/EjercicioFinalRepaso/src/main/java/org/example/App.java
+++ Acceso a Datos/Ejercicicos/1era evaluación/EjercicioFinalRepaso/src/main/java/org/example/App.java
@@ -10,12 +10,17 @@ public class App
     {
 
         Automovil auto1 = new Automovil("Seat","Leon",2010, 4);
-        Motocicleta moto1 = new Motocicleta("Yamaha", "245", "2020", 220);
+        Motocicleta moto1 = new Motocicleta("Yamaha", "245", 2020, 220);
+
+
+        auto1.arrancar();
+
+        moto1.arrancar();
 
 
 
 
 
-        );
+
     }
 }
