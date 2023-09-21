d07a91db0985e09fcc90c81f831d60b00def6c82
DVerdes <144799801+DVerdes@users.noreply.github.com>
2023-09-21 10:34:51 +0200

    calculadora

--- Programación Multimedia/flutter/flutter_application_1/lib/main - copia.dart
+++ Programación Multimedia/flutter/flutter_application_1/lib/main - copia.dart
@@ -3,18 +3,299 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(Dam_calculadora());
 }
+/*
+class Dam extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
 
-// APLICACIÓN DE CALCULADORA
+    return MaterialApp(
+      title: "Aplicación DAM",
+      home: Scaffold(appBar: AppBar(
+        title: Text("Barra de aplicación") ,
+      ),
+      body: Center(child: Icon(Icons.account_tree, size: 400, color: Colors.orange,)
+      ),
+      )
+    );
+
+  }
+
+}
+*/
+
+/*
+class Dam_2 extends StatelessWidget {
+
+  int cont = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Aplicación 2",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Barra de aplicación"),
+          ),
+          body: Center(
+            child: IconButton(
+              icon: Icon(Icons.accessibility_rounded),
+              onPressed: () {
+                cont++;
+                print("Hemos pulsado el icono " + cont.toString() + " veces");
+              },
+            ),
+          )),
+    );
+  }
+}
+
+*/
+
+/*
+class Dam_3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Aplicación 3",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Barra de aplicación"),
+        ),
+        body: Center(),
+        backgroundColor: Color.fromARGB(255, 192, 145, 2),
+        drawer: Drawer(),
+        endDrawer: Drawer(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.ac_unit),
+          onPressed: (){
+            print("Pulsador");
+          },
+        ),
+      ),
+    );
+  }
+}
+
+*/
+
+/*
+
+class Dam_4 extends StatelessWidget {
+  int cont1 = 0;
+  int cont2 = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "App 4",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Barra de aplicación"),
+          backgroundColor: Colors.indigo,
+          actions: [
+            IconButton( icon: Icon(Icons.account_balance_sharp), onPressed: pulsadorIzquierda),
+            IconButton( icon: Icon(Icons.rice_bowl), onPressed: pulsadorDerecha),
+            IconButton( icon: Icon(Icons.abc_outlined), onPressed: pulsadorContador)
+
+          ],
+        ),
+        body: Center(),
+      ),
+    );
+  }
+
+  void pulsadorIzquierda() {
+    print("Estas pulsando el botón de la izquierda");
+    cont1++;
+  }
+
+  void pulsadorDerecha() {
+        print("Estas pulsando el botón de la derecha");
+        cont2++;
+  }
+
+  void pulsadorContador(){
+    print("IZQ: "+cont1.toString());
+    print("DCH: "+cont2.toString());
+  }
+
+}
+
+*/
+
+/*
+
+class Dam_5 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Grandes columnas",
+      home: Scaffold(appBar: AppBar(
+        title: Text("Columnas") ,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text("Hola"),
+          Container(
+            color: Colors.amberAccent,
+            height: 100,
+            width: 100,
+          ),
+          Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLtgQ9FVUtqaWpzgwpRhTtDraQbCH-8pxnlQ&usqp=CAU",
+          height: 200,
+          width: 100,
+          ),
+        ],
+      )
+      ),
+    );
+
+  }
+
+}
+
+*/
+
+/*
+
+class Dam_6 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: "Filas",
+      home: Scaffold(appBar: AppBar(
+        title: Text("Filas"),
+      ),
+      body: Row(
+        children: [
+          Expanded(child: Text("Activar sonido")),
+          Icon(Icons.ac_unit, size: 50,),
+          Switch(value: false, onChanged: (value){
+
+          }),
+        ],
+      )
+      )
+    );
+
+  }
+
+}
+
+*/
+
+/*
+
+class Dam_7 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: "Filas y columnas",
+      home: Scaffold(appBar: AppBar(
+        title: Text("Combinación de filas y columnas") ,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                margin: EdgeInsets.all(10),
+                color: Colors.blueAccent,
+              ),
+                Container(
+                height: 100,
+                width: 100,
+                margin: EdgeInsets.all(10),
+                color: Colors.redAccent,
+              ),
+                Container(
+                height: 100,
+                width: 100,
+                                margin: EdgeInsets.all(10),
+
+                color: Colors.purpleAccent,
+              )
+            ],
+          ),
+          Divider(
+            color: Colors.transparent,
+
+          ),
+          Row( children: [
+              Container(
+                height: 100,
+                width: 100,
+                                margin: EdgeInsets.all(10),
+
+                color: Colors.greenAccent,
+              ),
+                Container(
+                height: 100,
+                width: 100,
+                                margin: EdgeInsets.all(10),
+
+                color: Colors.grey,
+              ),
+                Container(
+                height: 100,
+                width: 100,
+                                margin: EdgeInsets.all(10),
+
+                color: Colors.yellowAccent,
+              )
+            ],),
+                      Divider(
+                        color: Colors.transparent,
+                      ),
+
+          Row( children: [
+              Container(
+                height: 100,
+                width: 100,
+                                margin: EdgeInsets.all(10),
+
+                color: Colors.brown,
+              ),
+                Container(
+                height: 100,
+                width: 100,
+                                margin: EdgeInsets.all(10),
+                                child: Center(child: Text("Hey"),),
+
+                color: Colors.lightGreen,
+              ),
+                Container(
+                height: 100,
+                width: 100,
+                                margin: EdgeInsets.only(
+                                  left: 100
+                                ),
+                                child: Text("Hola",textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 20),),
+
+                color: Colors.blueAccent,
+              )
+            ],),
+
+        ],
+      ),
+      )
+    );
+
+  }
+
+}
+
+*/
 
 class Dam_calculadora extends StatelessWidget {
-  // VARIABLES
-  // Primer operando
   String op1 = "";
-  // Segundo operando
   String op2 = "";
-  // Tipo operador
   int operador = 0;
-  // Estado operador
   int operadorPulsado = 0;
   @override
   Widget build(BuildContext context) {
@@ -28,7 +309,6 @@ class Dam_calculadora extends StatelessWidget {
             body: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // Encabezado
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -41,11 +321,9 @@ class Dam_calculadora extends StatelessWidget {
                     )
                   ],
                 ),
-                // 1era fila botones
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // 7
                     InkWell(
                       onTap: () {
                         print("7");
@@ -74,7 +352,6 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // 8
                     InkWell(
                       onTap: () {
                         print("8");
@@ -100,10 +377,9 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // 9
                     InkWell(
                       onTap: () {
-                        print("9");
+                                                print("9");
 
                         if (operadorPulsado == 0) {
                           op1 += "9";
@@ -127,10 +403,10 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // Operador /
                     InkWell(
                       onTap: () {
-                        print("/");
+                                                print("/");
+
                         if (op1 != "") {
                           operador = 4;
                           operadorPulsado = 1;
@@ -153,14 +429,12 @@ class Dam_calculadora extends StatelessWidget {
                     ),
                   ],
                 ),
-                // 2nda fila botones
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // 4
                     InkWell(
                       onTap: () {
-                        print("4");
+                                                print("4");
                         if (operadorPulsado == 0) {
                           op1 += "4";
                         } else if (operadorPulsado == 1) {
@@ -186,10 +460,10 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // 5
                     InkWell(
                       onTap: () {
-                        print("5");
+                                                print("5");
+
                         if (operadorPulsado == 0) {
                           op1 += "5";
                         } else if (operadorPulsado == 1) {
@@ -212,10 +486,9 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // 6
                     InkWell(
                       onTap: () {
-                        print("6");
+                                                print("6");
 
                         if (operadorPulsado == 0) {
                           op1 += "6";
@@ -239,10 +512,9 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // Operador *
                     InkWell(
                       onTap: () {
-                        print("*");
+                                                print("*");
                         if (op1 != "") {
                           operador = 3;
                           operadorPulsado = 1;
@@ -265,14 +537,12 @@ class Dam_calculadora extends StatelessWidget {
                     ),
                   ],
                 ),
-                // 3era fila botones
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // 1
                     InkWell(
                       onTap: () {
-                        print("1");
+                                                print("1");
                         if (operadorPulsado == 0) {
                           op1 += "1";
                         } else if (operadorPulsado == 1) {
@@ -298,10 +568,9 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // 2
                     InkWell(
                       onTap: () {
-                        print("2");
+                                                print("2");
 
                         if (operadorPulsado == 0) {
                           op1 += "2";
@@ -325,10 +594,9 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // 3
                     InkWell(
                       onTap: () {
-                        print("3");
+                                                print("3");
                         if (operadorPulsado == 0) {
                           op1 += "3";
                         } else if (operadorPulsado == 1) {
@@ -351,10 +619,9 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // Operador -
                     InkWell(
                       onTap: () {
-                        print("-");
+                                                print("-");
                         if (op1 != "") {
                           operador = 2;
                           operadorPulsado = 1;
@@ -377,11 +644,9 @@ class Dam_calculadora extends StatelessWidget {
                     ),
                   ],
                 ),
-                // 4arta fila botones
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // CLEAR
                     InkWell(
                       onTap: () {
                         print("Clear");
@@ -406,10 +671,9 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // 0
                     InkWell(
                       onTap: () {
-                        print("0");
+                                                print("0");
                         if (operadorPulsado == 0) {
                           if (op1 != "") {
                             op1 += "0";
@@ -434,10 +698,9 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // Operador +
                     InkWell(
                       onTap: () {
-                        print("+");
+                                                print("+");
                         if (op1 != "") {
                           operador = 1;
                           operadorPulsado = 1;
@@ -458,31 +721,30 @@ class Dam_calculadora extends StatelessWidget {
                                 BorderRadius.all(Radius.circular(15.0))),
                       ),
                     ),
-                    // =
                     InkWell(
                       onTap: () {
+
                         if (operadorPulsado == 0) {
                           print("Error: selecciona un operador");
                         } else if (op1 == "" || op2 == "") {
                           print("Uno de los operandos no tiene valor");
                         } else {
-                          print("=");
-                          // Variables cálculo resultado
+                                                  print("=");
+
                           double num1 = double.parse(op1);
                           double num2 = double.parse(op2);
                           double resultado = 0;
                           int resultadoRedondeo = 0;
+
                           if (operador == 1) {
-                            resultadoRedondeo = ((num1 + num2) * 100).toInt();
-                            resultado = (resultadoRedondeo / 100).toDouble();
+                            resultado = num1 + num2;
                             print(num1.toString() +
                                 " + " +
                                 num2.toString() +
                                 " = " +
                                 resultado.toString());
                           } else if (operador == 2) {
-                            resultadoRedondeo = ((num1 - num2) * 100).toInt();
-                            resultado = (resultadoRedondeo / 100).toDouble();
+                            resultado = num1 - num2;
                             print(num1.toString() +
                                 " - " +
                                 num2.toString() +
@@ -505,8 +767,9 @@ class Dam_calculadora extends StatelessWidget {
                                 " = " +
                                 resultado.toString());
                           }
-                          // Reinicio variables
+
                           op1 = resultado.toString();
+
                           op2 = "";
                           operador = 0;
                           operadorPulsado = 2;
@@ -531,3 +794,35 @@ class Dam_calculadora extends StatelessWidget {
             )));
   }
 }
+
+
+/*
+  Container(
+    decoration: BoxDecoration(
+      color: Colors.yellow,
+      border: Border.all(
+        color: colors.black,
+        width: 6,
+      ),
+      borderRadius...
+    )
+  )
+
+*/
+
+
+/*
+
+El contenedor es estático, pero podemos hacer q ejecute algo mediante el widget INKWELL (lo convierte en dináminco)
+
+InkWell(
+  onTap: (){
+    print('7');
+  },
+  child: container(...)
+)
+
+
+
+
+*/
