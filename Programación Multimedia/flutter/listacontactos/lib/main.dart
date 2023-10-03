c6d3ed43f509698a5c0cc2c0291dbacfe18be1d9
DVerdes <144799801+DVerdes@users.noreply.github.com>
2023-10-03 18:22:31 +0200

    program multimedia

--- Programación Multimedia/flutter/listacontactos/lib/main.dart
+++ Programación Multimedia/flutter/listacontactos/lib/main.dart
@@ -6,53 +6,57 @@ void main() {
   runApp(Main());
 }
 
-class Main extends StatelessWidget{
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<Contacto> contactos = [
+      new Contacto("Pedro", "Martínez León", "24", "678345211",
+          "pman@gmail.com", "assets/images/pedro.png"),
+      new Contacto("Marta", "García Suárez", "35", "654789666",
+          "martagarcia@gmail.com", "assets/images/marta.png"),
+      new Contacto("Roberto", "Varela Hernández", "45", "654333555",
+          "robervar@gmail.com", "assets/images/roberto.png"),
+      new Contacto("Santiago", "Ramirez Ledesma", "23", "678345345",
+          "santiramirez@gmail.com", "assets/images/santiago.png"),
+      new Contacto("Asier", "Iraola Martín", "36", "654787898",
+          "asierirma@gmail.com", "assets/images/asier.png"),
+      new Contacto("Vanesa", "Villares Jurado", "31", "654787976",
+          "vanesavir@gmail.com", "assets/images/vanesa.png"),
+      new Contacto("Luís", "San Pedro Costas", "28", "654345234",
+          "luissan@gmail.com", "assets/images/luis.png"),
+      new Contacto("Lucía", "Rodríguez Rojo", "32", "654789345",
+          "luciarod@gmail.com", "assets/images/lucia.png"),
+      new Contacto("Humberto", "Barcia López", "56", "642123123",
+          "humbbar@gmail.com", "assets/images/humberto.png"),
+      new Contacto("Susana", "Piñeiro Pérez", "25", "675657345",
+          "susanapiñeiro@gmail.com", "assets/images/susana.png")
+    ];
 
-          List <Contacto> contactos = [new Contacto("Pedro", "Martínez León", "24", "678345211", "pman@gmail.com","../img/pedro.png"),
-          new Contacto("Marta", "García Suárez", "35", "654789666", "martagarcia@gmail.com",""),
-          new Contacto("Roberto", "Varela Hernández", "45", "654333555", "robervar@gmail.com",""),
-          new Contacto("Santiago", "Ramirez Ledesma", "23", "678345345", "santiramirez@gmail.com",""),
-          new Contacto("Asier", "Iraola Martín", "36", "654787898", "asierirma@gmail.com",""),
-          new Contacto("Vanesa", "Villares Jurado", "31", "654787976", "vanesavir@gmail.com",""),
-          new Contacto("Luís", "San Pedro Costas", "28", "654345234", "luissan@gmail.com",""),
-          new Contacto("Lucía", "Rodríguez Rojo", "32", "654789345", "luciarod@gmail.com",""),
-          new Contacto("Humberto", "Barcia López", "56", "642123123", "humbbar@gmail.com",""),
-          new Contacto("Susana", "Piñeiro Pérez", "25", "675657345", "susanapiñeiro@gmail.com","")];
-
-
-
-
-
-
-  
-      return MaterialApp(
-      title: "Proyecto 2", 
-      routes: {
-        "/segundaPagina": (BuildContext context) => SegundaClase(),
-      },
-      home: Scaffold(appBar: AppBar(title: Text("Agenda de contactos"),),
-      body: ListView.builder(itemCount: contactos.length, itemBuilder: (BuildContext context, index) {
-        final persona = contactos[index];
-         return ListTile(
-          title: Text((persona).getNombre),
-          leading: Icon(Icons.person),
-          onTap: () {
-            mostrarSegundaV(context, persona);
-          },
-          
-         );
-      }),)
-      
-     
-      
-    );
-    
+    return MaterialApp(
+        title: "Proyecto 2",
+        routes: {
+          "/segundaPagina": (BuildContext context) => SegundaClase(),
+        },
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Agenda de contactos"),
+          ),
+          body: ListView.builder(
+              itemCount: contactos.length,
+              itemBuilder: (BuildContext context, index) {
+                final persona = contactos[index];
+                return ListTile(
+                  title: Text((persona).getNombre),
+                  leading: Icon(Icons.person),
+                  onTap: () {
+                    mostrarSegundaV(context, persona);
+                  },
+                );
+              }),
+        ));
   }
-
 }
 
-mostrarSegundaV(BuildContext context, info){
+mostrarSegundaV(BuildContext context, info) {
   Navigator.of(context).pushNamed("/segundaPagina", arguments: info);
 }
--- Programación Multimedia/flutter/listacontactos/lib/páginas/segundaPagina.dart
+++ Programación Multimedia/flutter/listacontactos/lib/páginas/segundaPagina.dart
@@ -1,75 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:listacontactos/Contacto.dart';
 
-class SegundaClase extends StatelessWidget{
+class SegundaClase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Contacto contacto = ModalRoute.of(context)!.settings.arguments as Contacto;
 
-
-      Contacto contacto = ModalRoute.of(context)!.settings.arguments as Contacto;
-    
-    
     return Scaffold(
-      appBar: AppBar(title: Text("Contacto")),
-      body: Column(
-        children: [
-          
-             
-               Container(
-                // foto
-                height: 400,
-                color: Color.fromARGB(255, 223, 222, 222),
-                //child: Center(child: Icon(Icons.accessibility_new_sharp, size: 300),),
-                child: Center(child: Image.asset(contacto.rutaImg)),
-              
-              ),
-               
-              Container(
-                height: 150,
-                child: Center(child: Text(contacto.nombre + " " + contacto.apellidos, style: TextStyle(fontSize: 40, letterSpacing: 3, decorationThickness: 2.8)),),
-                margin: EdgeInsets.all(20) ,
-
-                
-              ),
-               Container(
-                height: 120,
-                child: Card(
-                child: Center(child: Text(contacto.telefono, style: TextStyle(fontSize: 30),),),
-                  color: Color.fromARGB(255, 210, 210, 211),
-                  margin: EdgeInsets.all(20) ,
-                  elevation: 20,
-                  surfaceTintColor: Color.fromARGB(255, 255, 255, 255),
-                  shadowColor: const Color.fromARGB(255, 0, 90, 164),
-                  
-              
-              
+        appBar: AppBar(title: Text("Contacto")),
+        body: Column(
+          children: [
+            Container(
+              // foto
+              height: 380,
+              margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+              color: Color.fromARGB(255, 255, 255, 255),
+              child: CircleAvatar(
+                radius: 150,
+                backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                child: Padding(
+                  padding: const EdgeInsets.all(3), // Border radius
+                  child: ClipOval(child: Image.asset(contacto.rutaImg)),
                 ),
               ),
-               Container(
-                height: 120,
-                child: Card(
-                  child: Center(child: Text(contacto.correo, style: TextStyle(fontSize: 20),),),
-
-                  color: Color.fromARGB(255, 210, 210, 211),
-                  margin: EdgeInsets.all(20) ,
-                  elevation: 20,
-                  surfaceTintColor: Color.fromARGB(255, 255, 255, 255),
-                  shadowColor: const Color.fromARGB(255, 0, 90, 164),
-                  
-              
-              
-                ),
+              //child: Center(child: Image.asset(contacto.rutaImg)),
+            ),
+            Container(
+              height: 150,
+              child: Center(
+                child: Text(contacto.nombre + " " + contacto.apellidos,
+                    style: TextStyle(
+                        fontSize: 40,
+                        letterSpacing: 3,
+                        decorationThickness: 2.8)),
               ),
-             
-            
-            ],
-          )
-        
-      );
-      
-      
-    
-    
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              height: 120,
+              child: Card(
+                child: Center(
+                  child: Text(
+                    contacto.telefono,
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                color: Color.fromARGB(255, 210, 210, 211),
+                margin: EdgeInsets.all(20),
+                elevation: 20,
+                surfaceTintColor: Color.fromARGB(255, 255, 255, 255),
+                shadowColor: const Color.fromARGB(255, 0, 90, 164),
+              ),
+            ),
+            Container(
+              height: 120,
+              child: Card(
+                child: Center(
+                  child: Text(
+                    contacto.correo,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                color: Color.fromARGB(255, 210, 210, 211),
+                margin: EdgeInsets.all(20),
+                elevation: 20,
+                surfaceTintColor: Color.fromARGB(255, 255, 255, 255),
+                shadowColor: const Color.fromARGB(255, 0, 90, 164),
+              ),
+            ),
+          ],
+        ));
   }
-
 }
