7bae3ebf50361aafe4ea4c36b5eb0592b93ac0ab
DVerdes <144799801+DVerdes@users.noreply.github.com>
2024-01-23 18:24:31 +0100

    ui

--- UI/NetBeansProjects/ejercicioLayouts/src/main/java/com/aulanosa/ejerciciolayouts/PrimaryController.java
+++ UI/NetBeansProjects/ejercicioLayouts/src/main/java/com/aulanosa/ejerciciolayouts/PrimaryController.java
@@ -1,12 +1,17 @@
 package com.aulanosa.ejerciciolayouts;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
+
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+
+    
+    
+    public void initialize(){
+       
+       
     }
+    
+    
+  
 }
