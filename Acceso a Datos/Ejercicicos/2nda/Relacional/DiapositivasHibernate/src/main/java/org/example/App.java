22bb6a4ef9ad21c55fcdab008774c638cd3a3831
DVerdes <144799801+DVerdes@users.noreply.github.com>
2024-02-05 14:42:27 +0100

    Update App.java

--- Acceso a Datos/Ejercicicos/2nda/Relacional/DiapositivasHibernate/src/main/java/org/example/App.java
+++ Acceso a Datos/Ejercicicos/2nda/Relacional/DiapositivasHibernate/src/main/java/org/example/App.java
@@ -2,7 +2,10 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.sql.Timestamp;
 
 
 /**
@@ -15,5 +18,16 @@ public class App
     {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
+
+        Timestamp  ts= new Timestamp(System.currentTimeMillis());
+        Tarea tarea1 = new Tarea(1,"Tarea 1","Prueba 1",ts,"Pendiente");
+        Tarea tarea2 = new Tarea(2,"Tarea 2","Prueba 2",ts,"En proceso");
+
+
+        Transaction transaction = session.beginTransaction();
+        session.save(tarea1);
+        session.save(tarea2);
+        transaction.commit();
+
     }
 }
