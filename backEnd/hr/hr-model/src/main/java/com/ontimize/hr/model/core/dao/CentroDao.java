5cb19b880c218696744fd651bc8ada8deec1a554
Daniel Verdes Martinez <dverdesm@gmail.com>
2024-05-17 10:42:45 +0200

    Centro Daos..

---
+++ backEnd/hr/hr-model/src/main/java/com/ontimize/hr/model/core/dao/CentroDao.java
@@ -0,0 +1,22 @@
+package com.ontimize.hr.model.core.dao;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+@Repository("CentroDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/CentroDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class CentroDao extends OntimizeJdbcDaoSupport {
+
+    public static final String ID_CENTRO = "ID_CENTRO";
+    public static final String ID_GESTOR = "ID_GESTOR";
+    public static final String DIR_CENTRO = "DIR_CENTRO";
+    public static final String NOMBRE_CENTRO = "NOMBRE_CENTRO";
+    public static final String CAPACIDAD = "CAPACIDAD";
+    public static final String CNT_CREATION_DATE = "CNT_CREATION_DATE";
+    public static final String CNT_DOWN_DATE = "CNT_DOWN_DATE";
+    public static final String CNT_PHOTO = "CNT_PHOTO";
+
+}
---
+++ backEnd/hr/hr-model/src/main/java/com/ontimize/hr/model/core/service/CentroService.java
@@ -0,0 +1,48 @@
+package com.ontimize.hr.model.core.service;
+
+import com.ontimize.hr.api.core.service.ICandidateService;
+import com.ontimize.hr.api.core.service.ICentroService;
+import com.ontimize.hr.model.core.dao.CandidateDao;
+import com.ontimize.hr.model.core.dao.CentroDao;
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("CentroService")
+@Lazy
+public class CentroService implements ICentroService {
+
+    @Autowired
+    private CentroDao centroDao;
+    @Autowired
+    private DefaultOntimizeDaoHelper daoHelper;
+
+    @Override
+    public EntityResult centroQuery(Map<String, Object> keyMap, List<String> attrList)
+            throws OntimizeJEERuntimeException {
+        return this.daoHelper.query(this.centroDao, keyMap, attrList);
+    }
+
+    @Override
+    public EntityResult centroInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+        return this.daoHelper.insert(this.centroDao, attrMap);
+    }
+
+    @Override
+    public EntityResult centroUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+            throws OntimizeJEERuntimeException {
+        return this.daoHelper.update(this.centroDao, attrMap, keyMap);
+    }
+
+    @Override
+    public EntityResult centroDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+        return this.daoHelper.delete(this.centroDao, keyMap);
+    }
+
+}
