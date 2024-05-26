2b5ba3f3b8e8a8b25bb2c704e38b2278513347f1
Daniel Verdes Martinez <dverdesm@gmail.com>
2024-05-26 12:23:31 +0200

    NEWUSER

--- backEnd/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewuserController.java
+++ backEnd/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewuserController.java
@@ -4,9 +4,14 @@
  */
 package com.mycompany.mavenproject1;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
 import static com.mycompany.mavenproject1.MainViewController.valoresLista;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
@@ -14,11 +19,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
 /**
  * FXML Controller class
@@ -26,6 +39,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
  * @author DVerd
  */
 public class NewuserController implements Initializable {
+    
+     @FXML
+    private Button subirImage;
     
      @FXML
     private TableView<Contacto> tViewContactos = new TableView<>();
@@ -51,6 +67,31 @@ public class NewuserController implements Initializable {
          
           @FXML
     private ChoiceBox comboGenero;
+          
+          @FXML
+          private TextField tfNombre;
+          
+                  @FXML
+          private TextField tfApellido1;
+                  
+                          @FXML
+          private TextField tfApellido2;
+                          
+                          final FileChooser f = new FileChooser();
+    @FXML
+    ImageView mv = new ImageView(); 
+    String url = "";
+
+                          
+                          
+                                  @FXML
+          private Slider sliDependencia = new Slider(0, 3, 0);
+                                  
+                                  
+                                  
+                                  
+                                          @FXML
+          private DatePicker dateNacimiento;
         
     static ObservableList<Contacto> valoresListaContacto = FXCollections.observableArrayList();
     
@@ -74,6 +115,19 @@ public class NewuserController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        sliDependencia.setMax(3);
+        sliDependencia.setMajorTickUnit(1);
+        sliDependencia.setMinorTickCount(0);
+        sliDependencia.setSnapToTicks(true);
+        sliDependencia.setShowTickMarks(true);
+        sliDependencia.setShowTickLabels(true);
+        
+        
+        String fechaInicial = "01-01-1950";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        dateNacimiento.setValue( LocalDate.parse(fechaInicial, formatter));
+
+        
         
         listaHabitaciones.add("Doble 12");
         listaCamas.add("A");
@@ -104,9 +158,46 @@ public class NewuserController implements Initializable {
     }    
     
      @FXML
-    private void crearUsuario() throws IOException {
+    private void crearUsuario() throws IOException, UnirestException {
+        String apellidos = tfApellido1.getText()+" "+tfApellido2.getText();
+        int dependenciaN= (int) Math.round(sliDependencia.getValue());
+        String dependencia = completarDependencia(String.valueOf(dependenciaN));
+        String genero = (String) comboGenero.getValue();
+        Date sqlDate = Date.valueOf(dateNacimiento.getValue());
         
+        System.out.println(apellidos+" "+dependencia+" "+genero+" "+sqlDate);
+        
+         String urlEndpoint = "http://localhost:33333/usuarios/usuario";
+        String body = "{\r\n    \"data\": \r\n        {\r\n                        \"FOTO_USUARIO\": \""+url+"\",\r\n                                    \"NOMBRE_USUARIO\": \""+tfNombre.getText()+"\",\r\n\r\n   \"APELLIDOS_USUARIO\": \""+apellidos+"\",\r\n\r\n    \"FECHA_NACIMIENTO\": \""+sqlDate+"\",\r\n\r\n   \"GENERO_USUARIO\": \""+genero+"\",\r\n\r\n  \"DEPENDENCIA_USUARIO\": \""+dependencia+"\"\r\n            \r\n        }\r\n}";
+        APIConnector.postMethod(urlEndpoint, body);
        
+    }
+    
+    
+     @FXML
+    private void subirImagen() throws IOException {
+        
+       subirImage.setOnAction((event) -> {
+            File file = f.showOpenDialog(new Window() {
+            });
+            if (file != null) {
+                url = file.toURI().toString();// only proceed, if file was chosen
+                Image img = new Image(file.toURI().toString());
+                mv.setImage(img);
+
+            }
+
+        });
+    }
+
+    private String completarDependencia(String string) {
+        String depend = "Grado "+string;
+        if(string.equals("0")){
+            return "Independiente";
+        }else{
+            return depend;
+        }
+        
     }
     
 }
